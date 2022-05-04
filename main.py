d1c9fa407496298b7233697083a6396b0a8cec9c
Jackiele123 <43102053+Jackiele123@users.noreply.github.com>
2022-05-04 00:29:55 -0400

    Set up

---
+++ main.py
@@ -0,0 +1,9 @@
+import quandl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
